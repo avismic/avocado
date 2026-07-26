513fbf02c0c6d019b10b6922371d4e241b56f9bc
avismic <abhishekanandsm0311@gmail.com>
2026-07-26 21:24:54 +0530

    milestone 10: made project vercel ready

--- server.js
+++ server.js
@@ -387,7 +387,9 @@ app.get("/api/fuel", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.VERCEL !== "1") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
